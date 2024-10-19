106953d3b4d0c64ee0800a76d43d8c38b7eb87c6
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-19 17:29:16 +0300

    feat(user-service): implement user creation logic in CreateUser function

--- backend/internal/services/user_service.go
+++ backend/internal/services/user_service.go
@@ -1,6 +1,10 @@
 package services
 
 import (
+	"errors"
+	"fmt"
+	"linktree-mohamedfadel-backend/internal/models"
+
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
@@ -23,4 +27,23 @@ func HashPassword(password string) (string, error) {
 
 func CheckPassword(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (s *UserService) SignUp(user models.User, password string) error {
+	if user.FullName == "" || user.Username == "" || password == "" {
+		return errors.New("required fields are missing")
+	}
+
+	var existingUser models.User
+	if err := s.db.Where("username = ?", user.Username).First(&existingUser).Error; err == nil {
+		return fmt.Errorf("username already exists: %v", err)
+	}
+
+	hashedPassword, err := HashPassword(password)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = hashedPassword
+
+	return s.db.Create(&user).Error
 }
