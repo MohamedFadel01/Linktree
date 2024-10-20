de4a0b755f20f5129193e3668bbcd8b52e7ad3e2
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-20 17:22:24 +0300

    feat(api-middleware): add JWT authentication middleware and adjusting GetUserProfileInfoHandler

---
+++ backend/internal/api/middleware/auth.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"linktree-mohamedfadel-backend/internal/utils"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ValidateJWTFromContext() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+			ctx.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		claims, err := utils.ValidateJWT(tokenString)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("username", claims.Username)
+		ctx.Next()
+	}
+}
