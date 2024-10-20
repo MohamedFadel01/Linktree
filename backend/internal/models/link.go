67962de23a0ac381a17d8ed84a8e20c81c11e144
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-20 22:00:08 +0300

    refactor(models): update foreign key and constraints for Link and User models

--- backend/internal/models/link.go
+++ backend/internal/models/link.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Link struct {
 	gorm.Model
 	Title     string    `json:"title"`
-	URL       string    `json:"url"`
+	URL       string    `json:"url" gorm:"unique"`
 	UserId    uint      `json:"user_id"`
 	Analytics Analytics `json:"analytics" gorm:"foreignKey:LinkID;constrain:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
