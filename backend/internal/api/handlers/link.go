87b2826f1187c2a9bfd88ccb050d3ca991487bac
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-21 17:20:47 +0300

    feat(api-linkhandler): add link update handler

--- backend/internal/api/handlers/link.go
+++ backend/internal/api/handlers/link.go
@@ -4,6 +4,7 @@ import (
 	"linktree-mohamedfadel-backend/internal/models"
 	"linktree-mohamedfadel-backend/internal/services"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -43,4 +44,33 @@ func (h *LinkHandler) CreateLinkHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Link created successfully"})
+}
+
+func (h *LinkHandler) UpdateLinkHandler(c *gin.Context) {
+	username, exists := c.Get("username")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	id := c.Param("id")
+	linkId, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var updatedLink models.Link
+	if err := c.ShouldBindJSON(&updatedLink); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	err = h.LinkService.UpdateLink(username.(string), linkId, updatedLink)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Link updated successfully"})
 }
