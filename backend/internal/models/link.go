cfbc6364c1ebd7274e61a00f05bb83807606c977
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-22 16:39:14 +0300

    fix(models): standardize UserID field in link and user models

--- backend/internal/models/link.go
+++ backend/internal/models/link.go
@@ -6,7 +6,7 @@ type Link struct {
 	ID        uint      `gorm:"primarykey"`
 	Title     string    `json:"title"`
 	URL       string    `json:"url" gorm:"unique"`
-	UserId    uint      `json:"user_id"`
+	UserID    uint      `json:"user_id"`
 	Analytics Analytics `json:"analytics" gorm:"foreignKey:LinkID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -7,7 +7,7 @@ type User struct {
 	FullName     string `json:"full_name"`
 	Username     string `json:"username" gorm:"unique"`
 	Bio          string `json:"bio"`
-	Links        []Link `json:"links" gorm:"foreignKey:UserId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Links        []Link `json:"links" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	PasswordHash string `json:"-"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
